a6924868189ca7cfab2e12e871da3493e5c85a2b
rayaapostolova <rajarus@abv.bg>
2020-11-13 19:08:14 +0000

    /

--- src/exercise6/Palindrome.java
+++ src/exercise6/Palindrome.java
@@ -3,7 +3,28 @@ package exercise6;
 import java.util.Scanner;
 
 public class Palindrome {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+  public static void main(String[] args) 
+  {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter number: ");
+    String number = input.next();
+    int parseInt = Integer.parseInt(number);
+    assert parseInt > 0 : "Wrong number!";
+    boolean isPalindrome = true;
+    String[] strArray = number.split("");
+    int[] intArray = new int[strArray.length];
+    for (int i = 0; i < strArray.length; i++) 
+    {
+      intArray[i] = Integer.parseInt(strArray[i]);
     }
+    for (int index = 0; index < intArray.length / 2; index++) 
+    {
+      if (intArray[index] != intArray[(intArray.length - 1) - index]) 
+      {
+        isPalindrome = false;
+      }
+    }
+    System.out.println("Palindrome: " + isPalindrome);
+  }
 }
+
